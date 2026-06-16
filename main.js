6cf92b5440d91f3961c1cebddcc29d1eeb89d5a1
Ben <benheaton@live.co.uk>
2026-06-16 11:59:03 +0100

    Added another feature

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
 let clickMe = document.getElementById('clickMe');
+let changeColour = document.getElementById('changeColour');
 let hiddenMessage = document.getElementById('hiddenMessage');
+
 clickMe.addEventListener('click', showMessage);
+changeColour.addEventListener('click', changeTextColour);
 
 function showMessage() {
     hiddenMessage.style.display = 'block';
 }
 
+function changeTextColour() {
+    document.querySelector('h1').style.color = 'green';
+    document.querySelector('h2').style.color = 'purple';
+}
